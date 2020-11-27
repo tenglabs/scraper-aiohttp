13492e465170ff5b584a4b4815d192856eff30af
tenglabs <tengoneotechnologies@gmail.com>
2020-11-28 01:45:17 +0200

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+import aiohttp
+import aiofiles
+
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url)
+        html = await response.text()
+        return html
+
+async def write_to_file(file, text):
+    async with aiofiles.open(file, 'w') as f:
+        await f.write(text)
+
+async def main(urls):
+    tasks = []
+    for url in urls:
+        file = f'{url.split("//")[-1]}.txt'
+        html = await  fetch(url)
+        tasks.append(write_to_file(file, html))
+
+    await asyncio.gather(*tasks)
+
+urls = ('https://python.org', 'https://stackoverflow.com')
+asyncio.run(main(urls))
